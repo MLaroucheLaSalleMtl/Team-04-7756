dc9caaac002af798d67e0b9408d4f45b62dfa82a
Ella Zhang <ellazhang37@gmail.com>
2019-01-30 20:28:57 -0500

    + Added Hero Character

---
+++ UnityRPG/Assets/Scripts/AttackControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackControl : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    Animator m_Animator;
+    CapsuleCollider m_Capsule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Capsule = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_Animator.SetTrigger("Attack");
+        }
+    }
+}
