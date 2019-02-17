0507d7a4631e2723578d8c68bc7b5204bf7e0d44
Viet Hoang Tran <hvtraanf@gmail.com>
2019-02-17 14:04:26 -0500

    main menu

---
+++ UnityRPG/Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingMenu : MonoBehaviour {
+
+    [SerializeField] private AudioMixer audioM;
+    [SerializeField] private AudioMixer audioS;
+
+    public void SetVol(float volume)
+    {
+        audioM.SetFloat("volume", volume);
+    }
+
+    public void SetSFX(float sfx)
+    {
+        audioS.SetFloat("sfx", sfx);
+    }
+
+    public void SetQuality(int qualIndex)
+    {
+        QualitySettings.SetQualityLevel(qualIndex);
+    }
+}
