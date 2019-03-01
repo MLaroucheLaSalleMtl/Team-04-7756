fb6636a4e13e94a9c16f7c38db30126a09074130
Sarkis T <sarkis308@gmail.com>
2019-03-01 13:13:49 -0500

    .

---
+++ UnityRPG/Assets/Scripts/AICharacterControl.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform target;                                    // target to aim for
+
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+        }
+
+
+        private void LateUpdate()
+        {
+            if (target != null)
+            {
+                if (Vector3.Distance(target.position, this.transform.position) > 6)
+                {
+                    agent.SetDestination(transform.position - target.position);
+                }
+                else
+                {
+                    agent.SetDestination(Vector3.zero);
+                }
+            }
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
---
+++ UnityRPG/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damageCaused;
+    public float projectileSpeed;
+
+    //[SerializeField] float projectileSpeed;
+    //float damageCaused = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Component damagableComponent = other.gameObject.GetComponent(typeof(IDamageable));
+        if (damagableComponent)
+        {
+            (damagableComponent as IDamageable).TakeDamage(damageCaused);
+        }
+    }
+}
