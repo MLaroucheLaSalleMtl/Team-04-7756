4dc1e4b3103534af4b66ac5bcc6b0f079147c9a9
Ella Zhang <ellazhang37@gmail.com>
2019-03-03 18:48:01 -0500

    + Sample Terrain

--- UnityRPG/Assets/Scripts/Projectile.cs
+++ UnityRPG/Assets/Scripts/Projectile.cs
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Projectile : MonoBehaviour
-{
-    private float damageCaused;
-    public float projectileSpeed;
-    GameObject player = null;
-
-    public void SetDamage(float damage)
-    {
-        damageCaused = damage;
-    }
-
-    //[SerializeField] float projectileSpeed;
-    //float damageCaused = 10f;
-    //private void Start()
-    //{
-    //    player = GameObject.FindGameObjectWithTag("Player");
-    //}
-
-    //private void Update()
-    //{
-    //    transform.rotation = Quaternion.LookRotation(player.transform.position - transform.position);
-    //}
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Component damagableComponent = other.gameObject.GetComponent(typeof(IDamageable));
-        if (damagableComponent)
-        {
-            (damagableComponent as IDamageable).TakeDamage(damageCaused);
-        }
-        Destroy(gameObject, 10f);
-    }
-}
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -58,4 +19,3 @@ public class Projectile : MonoBehaviour
         }
     }
 }
->>>>>>> master
