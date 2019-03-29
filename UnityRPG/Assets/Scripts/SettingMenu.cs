4d385e4253317697d1e2721dc4859ea648b1078b
Viet Hoang <hvtraanf@gmail.com>
2019-03-29 11:39:18 -0400

    Brightness, Fullscreen, Graphics
    Added Brightness and Fullscreen
    Temporary disabled Graphics

--- UnityRPG/Assets/Scripts/SettingMenu.cs
+++ UnityRPG/Assets/Scripts/SettingMenu.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
 public class SettingMenu : MonoBehaviour
 {
-
     [SerializeField] private AudioMixer audioM;
     [SerializeField] private AudioMixer audioS;
     [SerializeField] private Slider audioslider;
     [SerializeField] private Slider sfxslider;
     [SerializeField] private Slider BrightnessSlider;
-    private Rect sliderpos;
+    [SerializeField] private Toggle windowToggle;
+    [SerializeField] private PostProcessProfile profile;
+
+    private float currentVolume;
+    private float currentSFX;
     private float rbgValue = 0.5f;
 
     public void Start()
     {
-        PlayerPrefs.GetFloat("set volume", 0);
-        PlayerPrefs.GetFloat("set sfx", 0);
-        PlayerPrefs.GetInt("set qual", 0);
-        sliderpos = new Rect(BrightnessSlider.transform.position.x, BrightnessSlider.transform.position.y, 160f, 20f);
+        currentVolume = PlayerPrefs.GetFloat("set volume", audioslider.value);
+        currentSFX = PlayerPrefs.GetFloat("set sfx", audioslider.value);
+        sfxslider.value = currentSFX;
+        BrightnessSlider.value = profile.GetSetting<ColorGrading>().postExposure.value;
     }
 
     public void SetVol(float volume)
@@ -33,29 +37,31 @@ public class SettingMenu : MonoBehaviour
     public void SetSFX(float sfx)
     {
         audioS.SetFloat("sfx", sfx);
-        PlayerPrefs.SetFloat("set sfx", sfx);
+        PlayerPrefs.SetFloat("set volume", sfx);
         PlayerPrefs.Save();
     }
 
     public void SetQuality(int qualIndex)
     {
         QualitySettings.SetQualityLevel(qualIndex);
-        PlayerPrefs.SetInt("set qual", qualIndex);
-        PlayerPrefs.Save();
     }
 
-    //public void OnGUI()
-    //{
-    //    rbgValue = GUI.HorizontalSlider(sliderpos, rbgValue, 0f, 1.0f);
-    //}
+    public void SetFullScreen()
+    {
+        if (windowToggle.isOn)
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+            Debug.Log("Fullscreen is OFF");
+        }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+            Debug.Log("Fullscreen is ON");
+        }
+    }
 
-    ////public void SetBrightness()
-    ////{
-    ////    rbgValue = BrightnessSlider.value;
-    ////}
-
-    //public void Update()
-    //{
-    //    RenderSettings.ambientLight = new Color(rbgValue, rbgValue, rbgValue, 1);
-    //}
+    public void SetBrightness()
+    {
+        profile.GetSetting<ColorGrading>().postExposure.value = BrightnessSlider.value;
+    }    
 }
